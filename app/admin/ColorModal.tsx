5b5400aa10850fb1df8be84b032d5fd2f3c62acc
Roberto Carlos <rocasaav@gmail.com>
2026-09-02 23:23:02 -0600

    Cambio ruta relativa

--- app/admin/ColorModal.tsx
+++ app/admin/ColorModal.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect, useCallback } from 'react';
-import { supabase } from '@/lib/supabase';
+import { supabase } from "../../lib/supabase";
 import Image from 'next/image';
 
 interface ColorItem {
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect, useCallback } from 'react';
-import { supabase } from '@/lib/supabase';
+import { supabase } from "../../lib/supabase";
 import Image from 'next/image';
 import Link from 'next/link';
 import ColorModal from './ColorModal';
