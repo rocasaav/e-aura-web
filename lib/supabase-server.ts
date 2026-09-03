2ef1dce6d1c75fc58697435a07565d51a3a5b13b
Roberto Carlos <rocasaav@gmail.com>
2026-09-02 21:59:19 -0600

    fix: supabase-server SSR compability for Vercel

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 
 export function createServerSupabase() {
-  const cookieStore = cookies();
+  const cookieStore = cookies(); // 👈 IMPORTANTE: debe ejecutarse
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
